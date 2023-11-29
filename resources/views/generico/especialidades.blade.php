0642b0e5c9253d19305c5f77b8f23c9a74a2845d
Wkana PC <willian.kana@tecsup.edu.pe>
2023-11-28 21:13:27 -0500

    proyecto env ignorado

--- resources/views/generico/especialidades.blade.php
+++ resources/views/generico/especialidades.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css" />
-  <title>Especialidades</title>
-</head>
-<body>
-
 <div class="container mt-5">
   <h1 class="text-center card-title">Especialidades</h1>
 
