9d2347a6264db84598aeead59a6fbe15e48a5d27
Wkana PC <willian.kana@tecsup.edu.pe>
2023-12-04 16:02:21 -0500

    nosotros arreglado a medias

--- resources/views/generico/nosotros.blade.php
+++ resources/views/generico/nosotros.blade.php
@@ -12,9 +12,10 @@
             <h2 class="text-light" style="font-size: 3em; margin-bottom: 100px">
               a nuestra Clínica
             </h2>
-            <div class="row row-cols-1 row-cols-sm-2">
+
+            <div class="row row-cols-1 row-cols-lg-2">
               <div class="d-flex">
-                <div style="margin-right: 30px">
+                <div class="d-flex align-items-center" style="margin-right: 30px;">
                   <img
                     src="assets/img/index/especialidades_icon/heart-pulse.svg"
                     class="card-img-top icon-especializacion"
@@ -38,7 +39,7 @@
                 </div>
               </div>
               <div class="d-flex">
-                <div style="margin-right: 30px">
+                <div class="d-flex align-items-center" style="margin-right: 30px">
                   <img
                     src=" assets/img/index/especialidades_icon/heart-pulse.svg"
                     class="card-img-top icon-especializacion"
@@ -64,86 +65,123 @@
           </div>
         </div>
       </section>
+
+
       <!-- Visión y Misión -->
-      <section class="bg-seccion b-nosotros">
-        <div class="container-fluid"></div>
+      <section class="">
         <div class="container nosotros align-items-center d-flex">
-          <div>
-            <h2 class="titulo" style="font-size: 3em; margin-bottom: 50px">
-              Somos la primera clínica...
-            </h2>
-            <div class="row">
-              <div>
-                <h5 class="titulo" style="font-size: 1.5em">
-                  Nuestra Misión
-                </h5>
-                <div class="card-body">
-                  <p class="card-text" style="font-size: 1.2em; margin-bottom: 20px">
-                    Brindar atención a nuestros pacientes para devolverles la salud y la 
-                    felicidad a través de una atención comprometida, médicos profesionales y 
-                    equipamiento de primera calidad.
-                  </p>
+          <div class="row row-cols-1 row-cols-lg-2">
+
+            <div class="d-flex">
+
+              <div class="row">
+                <h2 class="titulo" style="font-size: 3em; margin-bottom: 50px">
+                  Somos la primera clínica...
+                </h2>
+                <div>
+                  <h5 class="titulo" style="font-size: 1.5em">
+                    Nuestra Misión
+                  </h5>
+                  <div class="card-body">
+                    <p class="card-text" style="font-size: 1.2em; margin-bottom: 20px">
+                      Brindar atención a nuestros pacientes para devolverles la salud y la 
+                      felicidad a través de una atención comprometida, médicos profesionales y 
+                      equipamiento de primera calidad.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <h5 class="titulo" style="font-size: 1.5em; margin-top: 20px">
+                    Nuesta Visión
+                  </h5>
+                  <div class="card-body">
+                    <p class="card-text" style="font-size: 1.2em">
+                      Liderar el servicio de salud en nuestra ciudad, diferenciándonos por nuestros 
+                      valores, calidez humana, calidad y responsabilidad social para con todos nuestros clientes.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div>
-                <h5 class="titulo" style="font-size: 1.5em; margin-top: 20px">
-                  Nuesta Visión
-                </h5>
-                <div class="card-body">
-                  <p class="card-text" style="font-size: 1.2em">
-                    Liderar el servicio de salud en nuestra ciudad, diferenciándonos por nuestros 
-                    valores, calidez humana, calidad y responsabilidad social para con todos nuestros clientes.
-                  </p>
-                </div>
-              </div>
+
             </div>
-            <div class="container-fluid" style="height: 50px"></div>
-          </div>
-          <div>
-            <img
+
+            <div class="d-flex">
+
+              <img
               src="assets/img/index/comentarios-bg.jpg"
               class="card-img-top icon-especializacion"
               style="width:500px; height:100%; margin-left: 50px"
-            />
+              />
+
+            </div>
+
+          </div>
+
+          <div>
+            
+            
+            <div class="container-fluid" style="height: 50px"></div>
+          </div>
+          <div>
+            
           </div>
         </div>
       </section>
+
+
       <!-- Historia -->
-      <section class="bg-seccion b-nosotros2">
+      <section class="">
         <div class="container-fluid"></div>
         <div class="container nosotros align-items-center d-flex">
-          <div>
-            <img
+
+          <div class="row row-cols-1 row-cols-lg-2">
+
+            <div class="d-flex order-lg-2">
+
+              <img
               src="assets/img/index/comentarios-bg.jpg"
               class="card-img-top icon-especializacion"
               style="width:500px; height:100%; margin-right: 50px"
-            />
-          </div>
-          <div>
-            <h2 class="titulo" style="font-size: 3em; margin-bottom: 50px">
-              Nuestra Historia Médica
-            </h2>
-            <div class="row">
-              <div>
-                <h5 class="titulo" style="font-size: 1.5em">
-                  Nuestra Misión
-                </h5>
-                <div class="card-body">
-                  <p class="card-text" style="font-size: 1.2em; margin-bottom: 20px">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, laboriosam autem! Provident sint, 
-                    harum quas iure dolorem facilis esse dolores dolorum modi! Eos maxime id culpa praesentium eveniet voluptatum et. 
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt ut voluptatem harum sit amet illum qui magnam. 
-                    Dolorum consectetur commodi, omnis, delectus ipsum totam, fuga aliquid quis optio quasi cumque?. Lorem ipsum dolor 
-                    sit amet consectetur adipisicing elit. Debitis vel odit consequuntur, rem, assumenda, quos dolores possimus esse obcaecati 
-                    voluptatibus nostrum perferendis. Commodi mollitia dolorem libero iure in quae aliquid. Lorem ipsum dolor sit amet consectetur 
-                    adipisicing elit. Quidem in recusandae velit! Soluta inventore magni incidunt vel dolore fugiat, ex quam quas unde deleniti iste 
-                    distinctio. Ut et blanditiis laboriosam!
-                  </p>
+              />
+
+            </div>
+
+            <div class="d-flex order-lg-1">
+
+              
+              <div class="row">
+                <h2 class="titulo" style="font-size: 3em; margin-bottom: 50px">
+                  Nuestra Historia Médica
+                </h2>
+                <div>
+                  <h5 class="titulo" style="font-size: 1.5em">
+                    Nuestra Misión
+                  </h5>
+                </div>
+                <div>
+                  <div class="card-body">
+                    <p class="card-text" style="font-size: 1.2em; margin-bottom: 20px">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, laboriosam autem! Provident sint, 
+                      harum quas iure dolorem facilis esse dolores dolorum modi! Eos maxime id culpa praesentium eveniet voluptatum et. 
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt ut voluptatem harum sit amet illum qui magnam. 
+                      Dolorum consectetur commodi, omnis, delectus ipsum totam, fuga aliquid quis optio quasi cumque?. Lorem ipsum dolor 
+                      sit amet consectetur adipisicing elit. Debitis vel odit consequuntur, rem, assumenda, quos dolores possimus esse obcaecati 
+                      voluptatibus nostrum perferendis. Commodi mollitia dolorem libero iure in quae aliquid. Lorem ipsum dolor sit amet consectetur 
+                      adipisicing elit. Quidem in recusandae velit! Soluta inventore magni incidunt vel dolore fugiat, ex quam quas unde deleniti iste 
+                      distinctio. Ut et blanditiis laboriosam!
+                    </p>
+                  </div>
                 </div>
               </div>
+
+             
+
+              <div class="container-fluid" style="height: 50px"></div>
+
             </div>
-            <div class="container-fluid" style="height: 50px"></div>
+
           </div>
+
         </div>
       </section>
     </main>
