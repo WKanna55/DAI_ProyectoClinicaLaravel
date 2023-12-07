c260022330a707d63fb0f9b2d09e1fbb1868c5a5
Gabriel Ccama <gabriel.ccama@tecsup.edu.pe>
2023-12-07 10:53:17 -0500

    Interfaces de cita

---
+++ resources/views/paciente/agendar_horario.blade.php
@@ -0,0 +1,33 @@
+@extends ('layouts.plantilla')
+@section ('content')
+
+    <main>
+        <section>
+            <div class="container-agendar custom-px-10">
+                <h1 class="text-center">Reserva tu Cita</h1>
+                <div class="text-center mt-5">
+                    <div class="step-circle bg-danger"></div>
+                    <div class="step-circle bg-danger"></div>
+                    <div class="step-circle bg-danger"></div>
+                    <div class="step-circle bg-danger"></div>
+                </div>
+                <h2 class="pt-5 pb-2">Horario</h2>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Selecciona el horario</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>   
+                <h2 class="pt-5 text-center">S/ 200.00</h2>
+                <div class="pt-5 text-center">
+                    <button type="button" class="btn btn-success"
+                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                        Pagar
+                    </button>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- partial -->
+
+@endsection
