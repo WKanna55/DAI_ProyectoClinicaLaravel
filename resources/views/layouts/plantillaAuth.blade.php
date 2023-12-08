ec5a6700a6ec3609c5ac62022ed6865a5578145e
Kevin Caya <kevin.caya@tecsup.edu.pe>
2023-12-08 12:59:11 -0500

    atencion

--- resources/views/layouts/plantillaAuth.blade.php
+++ resources/views/layouts/plantillaAuth.blade.php
@@ -25,18 +25,6 @@
       
     </main>
     <!-- Bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script >
-        function enviarFormulario() {
-          // Obtén el formulario por su ID
-          var formulario = document.getElementById('formulario-ruta');
-
-          // Simula el clic en el botón de submit del formulario
-          formulario.querySelector('button[type="submit"]').click();
-          
-          // Devuelve false para evitar que el enlace realice su comportamiento predeterminado
-          return false;
-      }
-    </script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
   </body>
 </html>
