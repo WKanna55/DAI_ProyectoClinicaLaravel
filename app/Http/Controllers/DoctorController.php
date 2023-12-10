01ff839a57d9ad4672cdf9d9870a478e3834839c
WKanna55 <143762970+WKanna55@users.noreply.github.com>
2023-12-10 18:54:28 -0500

    Update DoctorController.php
    Update DoctorController.php eso

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -183,34 +183,4 @@ Si tienes alguna pregunta o necesitas más información, no dudes en contactarno
 
         return redirect()->route('citas');
     }
-
-    public function guardarAtencion(Request $request)
-    {
-        // Obtener los datos del formulario
-        $alergias = $request->input('alergias');
-        $sintomas = $request->input('sintomas');
-        $operaciones = $request->input('operaciones');
-        $valoracion = $request->input('valoracion');
-        $medicamentos = $request->input('medicamentos');
-        $instrucciones = $request->input('instrucciones');
-
-        $receta = "Medicamentos: " . $medicamentos . "\nInstrucciones: " . $instrucciones;
-
-        // Guardar en la base de datos
-        $diagnosis = new diagnoses();
-        $diagnosis->alergias = $alergias;
-        $diagnosis->sintomas = $sintomas;
-        $diagnosis->OperAnteriores = $operaciones;
-        $diagnosis->valoracion = $valoracion;
-        $diagnosis->receta = $receta;
-        $diagnosis->save();
-
-        // Lógica para enviar por Green API a WhatsApp del paciente
-        // ...
-
-        // Redirigir o enviar una respuesta de éxito
-        return redirect()->back()->with('success', 'Diagnóstico guardado correctamente.');
-    }
-
-
 }
