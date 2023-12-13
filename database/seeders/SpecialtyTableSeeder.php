bc92ebed2695cd43d6a25094fa23f4ca6f52f580
diegodolu <126405033+diegodolu@users.noreply.github.com>
2023-12-13 11:12:38 -0500

    cambio seeder user y specialty, dnis y precios

--- database/seeders/SpecialtyTableSeeder.php
+++ database/seeders/SpecialtyTableSeeder.php
@@ -15,42 +15,42 @@ class SpecialtyTableSeeder extends Seeder
     {
         $specialty1 = new Specialty;
         $specialty1-> nombre = "Medicina General";
-        $specialty1-> costo = 100;
+        $specialty1-> costo = 40;
         $specialty1-> descripcion = "atender cita de medicina general";
         $specialty1-> img_source = "medicinaGenral.jpg";
         $specialty1->save();
 
         $specialty2 = new Specialty;
         $specialty2-> nombre = "Ginecología";
-        $specialty2-> costo = 150;
+        $specialty2-> costo = 100;
         $specialty2-> descripcion = "atender cita de ginecologia";
         $specialty2-> img_source = "ginecologia.jpg";
         $specialty2->save();
 
         $specialty3 = new Specialty;
         $specialty3-> nombre = "Dermatología";
-        $specialty3-> costo = 200;
+        $specialty3-> costo = 90;
         $specialty3-> descripcion = "atender cita de dermatologia";
         $specialty3-> img_source = "dermatologia.jpg";
         $specialty3->save();
 
         $specialty4 = new Specialty;
         $specialty4-> nombre = "Odontología";
-        $specialty4-> costo = 250;
+        $specialty4-> costo = 60;
         $specialty4-> descripcion = "atender cita odontologia";
         $specialty4-> img_source = "odontologia.jpg";
         $specialty4->save();
 
         $specialty5 = new Specialty;
         $specialty5-> nombre = "Gastroenterología";
-        $specialty5-> costo = 300;
+        $specialty5-> costo = 90;
         $specialty5-> descripcion = "atender cita gastroenterologia";
         $specialty5-> img_source = "gastroenterologia.jpg";
         $specialty5->save();
 
         $specialty6 = new Specialty;
         $specialty6-> nombre = "Oftalmología";
-        $specialty6-> costo = 350;
+        $specialty6-> costo = 80;
         $specialty6-> descripcion = "atender cita oftalmolofia";
         $specialty6-> img_source = "oftalmologia.jpg";
         $specialty6->save();
